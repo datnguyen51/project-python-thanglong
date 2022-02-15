0cca0518c5594102bc539b762fd527e581638770
nvdat519 <nguyenvandat511999@gmail.com>
2022-02-15 17:17:54 +0700

    update

--- apps/api/students/routes.py
+++ apps/api/students/routes.py
@@ -11,12 +11,12 @@ from apps.api.students.model import Student
 @login_required
 def students():
     records_student = Student.query.all()
-    print(records_student)
+    print(records_student[0].id)
 
     return render_template('students/students.html', student=records_student)
 
 
-@blueprint.route('/add_student', methods=['GET', 'POST'])
+@blueprint.route('/add-student', methods=['GET', 'POST'])
 @login_required
 def add_student():
     if request.method == 'GET':
@@ -75,3 +75,72 @@ def add_student():
 
         return render_template('students/student-info.html')
     # return render_template('students/student-info.html')
+
+
+@blueprint.route('/info-student/<id>', methods=['GET', 'PUT'])
+@login_required
+def student_info(id):
+    if request.method == 'GET':
+        record_student = Student.query.filter_by(
+            id=id).filter_by(
+            deleted=False).first()
+
+        # if not record_student:
+
+        return render_template('students/student-info.html', student=record_student)
+
+    if request.method == 'PUT':
+        avatar = request.files['avatar']
+        code = request.form['code']
+        name = request.form['name']
+        address = request.form['address']
+        gender = request.form['gender']
+        birthday = request.form['birthday']
+        email = request.form['email']
+        phone_number = request.form['phone_number']
+        identification = request.form['identification']
+        health_insurance = request.form['health_insurance']
+        student_class = request.form['student_class']
+        student_major = request.form['student_major']
+
+        if not code:
+            return render_template('students/student-info.html', has_error='Code is empty!')
+        if not name:
+            return render_template('students/student-info.html', has_error='Name is empty!')
+        if not address:
+            return render_template('students/student-info.html', has_error='Address is empty!')
+        if not gender:
+            return render_template('students/student-info.html', has_error='Gender is empty!')
+        if not birthday:
+            return render_template('students/student-info.html', has_error='Birthday is empty!')
+        if not email:
+            return render_template('students/student-info.html', has_error='Email is empty!')
+        if not phone_number:
+            return render_template('students/student-info.html', has_error='Phone number is empty!')
+        if not student_class:
+            return render_template('students/student-info.html', has_error='Class is empty!')
+        if not student_major:
+            return render_template('students/student-info.html', has_error='Major is empty!')
+
+        record_student = Student.query.filter_by(
+            code=code).filter_by(
+            deleted=False).first()
+
+        if record_student:
+            return render_template('students/student-info.html', has_error='Student code already exists!')
+
+        record_student = Student.query.filter_by(
+            email=email).filter_by(
+            deleted=False).first()
+
+        if record_student:
+            return render_template('students/student-info.html', has_error='Student email already exists!')
+
+        record_student = Student.query.filter_by(
+            id=id).filter_by(
+            deleted=False).first()
+
+        # db.session.add(record_student)
+        db.session.commit()
+
+        return render_template('students/student-info.html')
